71929bef6a7d2b71a44a616c346dcd1f30e88c9a
Nick Prokesch <nicholas.prokesch@clover.com>
2024-09-20 14:24:59 -0500

    Initial commit

---
+++ lua/plugins/user.lua
@@ -0,0 +1,11 @@
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+---@type LazySpec
+return {
+
+	{ "tpope/vim-repeat" },
+	{ "jamestthompson3/nvim-remote-containers" },
+
+	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+}
---
+++ lua/polish.lua
@@ -0,0 +1,19 @@
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.g.ruby_host_prog = "$HOME/.asdf/installs/ruby/3.3.4/bin/neovim-ruby-host"
