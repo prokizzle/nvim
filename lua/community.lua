169e4cbd011fd5e8a06baaf4471b41fcb690e6b9
Nick Prokesch <nicholas.prokesch@clover.com>
2024-10-11 17:21:30 -0500

    Use nord theme

--- lua/community.lua
+++ lua/community.lua
@@ -41,6 +41,7 @@ return {
     { import = "astrocommunity.color.twilight-nvim" },
     { import = "astrocommunity.colorscheme.everforest" },
     { import = "astrocommunity.colorscheme.nord-nvim" },
+    { import = "astrocommunity.colorscheme.nordic-nvim" },
     { import = "astrocommunity.colorscheme.kanagawa-nvim" },
     { import = "astrocommunity.colorscheme.catppuccin" },
     { import = "astrocommunity.colorscheme.github-nvim-theme" },
